ecdbed417bb19f1945ee24e18fd383537573b845
kozhushna <k.kozhushna@gmail.com>
2023-01-31 18:34:02 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsListEl = document.querySelector('#ingredients');
+
+const createIngredientsList = ingredients => {
+  return ingredients.map(ingredient => {
+    const itemEl = document.createElement('li');
+    itemEl.textContent = ingredient;
+    itemEl.classList.add('item');
+
+    return itemEl;
+  });
+};
+
+const elements = createIngredientsList(ingredients);
+
+ingredientsListEl.append(...elements);
