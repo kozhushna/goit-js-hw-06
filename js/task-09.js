94db9f006f7e848e56549329ca8c0f97a69c39f2
kozhushna <k.kozhushna@gmail.com>
2023-02-03 13:18:13 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const refs = {
+  spanEl: document.querySelector('.color'),
+  btnEl: document.querySelector('.change-color'),
+};
+
+refs.btnEl.addEventListener('click', onBackgroundColorChange);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onBackgroundColorChange() {
+  const backgroundColor = getRandomHexColor();
+  document.body.style.backgroundColor = backgroundColor;
+  refs.spanEl.textContent = backgroundColor;
 }
