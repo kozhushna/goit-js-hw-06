b0bde9ed73e0606a819bd933a0a4c7a873123a80
kozhushna <k.kozhushna@gmail.com>
2023-02-03 12:46:21 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const target = event.currentTarget;
+  const formData = {
+    email: target.elements.email.value.trim(),
+    password: target.elements.password.value.trim(),
+  };
+  if (formData.email === '' || formData.password === '') {
+    alert('Всі поля повинні бути заповнені');
+    return;
+  }
+
+  console.log(formData);
+  form.reset();
+}
